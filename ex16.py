d6de53cf2bd87a019f747849f804759b9beaca6b
CarlMcAteer <carlmc12@gmail.com>
2015-09-27 17:39:11 +0100

    added .gitignore

--- ex16.py
+++ ex16.py
@@ -12,22 +12,30 @@ print "Opening the file..."
 target = open(filename, 'w')
 
 print "Truncating the file. Goodbye!"
-target.truncate()
+# Ereasing the contence of the file that was named in the opening the argument.
+# Not really required since
+# target.truncate()
 
 print "Now I'm going to ask you for three lines."
 
+# Declare a variable to a line of raw_input from the user x3.
 line1 = raw_input("line 1:")
 line2 = raw_input("line 2:")
 line3 = raw_input("line 3:")
 
 print "I'm ging to write these to a file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+# Rewrite the w
+target.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
+# Write a line in the new file, with the variables declared earlier.
+# Drops down to a new line in the new file.
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
 print "And finally, we close it."
+# Closes and saves the new file.
 target.close()
